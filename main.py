b5503f60ab2f8303e77ed5499c30e24e0f99ad66
giimiks <fakinafakuka@gmail.com>
2024-04-11 15:27:34 +0200

    main menu + death menu revamp, score system proto

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 import time
 
 import pygame
+import pygame_menu
 
 #konstanty
 DISP_SIZE = (720,540) #velikost obrazovky
@@ -18,97 +19,95 @@ GROUND_Y = DISP_SIZE[1]-200 #podlaha, na které bude postava stát a skákat
 JUMP_HEIGHT = 100  # výška skoku
 GRAVITY = 1.75  # gravitace
 
+
 #funkce main; pokud bych jinde importoval main.py, nespustí se mi hra znovu
 def main():
       #základní setup hry
       pygame.init()
-      pygame.font.init()
-      inMenu = True
-      disp = pygame.display.set_mode(DISP_SIZE)
-      gameClock = pygame.time.Clock()
-
-      font = pygame.font.SysFont('Comic Sans MS', 40)
-      virusImage = pygame.image.load('assets/virus.png')
-      skyImage = pygame.image.load('assets/sky.png')
-      cubeImage = pygame.image.load('assets/cube.png')
-      groundRect = pygame.Rect(0,DISP_SIZE[1]-160,DISP_SIZE[0],160)
-      groundOverlay = pygame.Rect(0,DISP_SIZE[1]-160,DISP_SIZE[0],60)
-      resButton = pygame.Rect(100,270,150,100)
       
-      cubeX = DISP_SIZE[0]//3
-      cubeY = GROUND_Y/1.5
-      cubeVelocity = 0
-      jumping = True
-      obstX = DISP_SIZE[0]
-      running = True
-      held = False
-      dead = False
-      inMenu = False 
+      disp = pygame.display.set_mode(DISP_SIZE)
+      
+      def game():
+            score = 0
 
-      #hlavní smyčka hry
-      while running:
+            pygame.font.init()
+            inMenu = True
             
-            #hra projde každý event při jedné herní smyčce
-            for event in pygame.event.get():
-                  if event.type == pygame.QUIT:
-                        running = False
-                  #kontrola stisknutí a držení levého tlačítka na myši
-                  if not inMenu:
+            gameClock = pygame.time.Clock()
+
+            font = pygame.font.SysFont('Comic Sans MS', 40)
+            virusImage = pygame.image.load('assets/virus.png')
+            skyImage = pygame.image.load('assets/sky.png')
+            cubeImage = pygame.image.load('assets/cube.png')
+            groundRect = pygame.Rect(0,DISP_SIZE[1]-160,DISP_SIZE[0],160)
+            groundOverlay = pygame.Rect(0,DISP_SIZE[1]-160,DISP_SIZE[0],60)
+            resButton = pygame.Rect(100,270,150,100)
+            
+            cubeX = DISP_SIZE[0]//3
+            cubeY = GROUND_Y/1.5
+            cubeVelocity = 0
+            jumping = True
+            obstX = DISP_SIZE[0]
+            running = True
+            held = False
+            inMenu = False 
+
+            #hlavní smyčka hry
+            while running:
+                  score+=1
+                  #hra projde každý event při jedné herní smyčce
+                  for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                              running = False
+                        #kontrola stisknutí a držení levého tlačítka na myši
                         if event.type == pygame.MOUSEBUTTONDOWN:
-                              held = True
+                                    held = True
                         elif event.type == pygame.MOUSEBUTTONUP:
-                              held = False
-                  else:
-
-                        pos = pygame.mouse.get_pos()
-                        if event.type == pygame.MOUSEBUTTONUP and resButton.collidepoint(pos):
-                              obstX = DISP_SIZE[0] 
-                              time.sleep(0.5)
-                              dead = False
-                              inMenu = False
-                              
-                              
-            #pokud hráč není ve vzduchu a levé tlačítko je stisknuto/podrženo, postava vyskočí a sníží svou vertikální rychlost
-            if not jumping and cubeY == GROUND_Y:
-                  if held:
-                        jumping = True
-                        cubeVelocity = -20
-            if jumping:
-                  cubeY += cubeVelocity
-                  cubeVelocity += GRAVITY
-                  #pokud se postava dotýká země, postava se nenachází ve vzduchu a vertikální rychlost je vynulována
-                  if cubeY >= GROUND_Y:
-                        cubeY = GROUND_Y
-                        jumping = False
-                        cubeVelocity = 0    
-            if obstX < 0:
-                  obstX = DISP_SIZE[0]
+                                    held = False
             
-            
-            #zobrazení všech elementů na obrazovku
-            if not dead:
-                  resButton = pygame.Rect(0,0,0,0)
+                  #pokud hráč není ve vzduchu a levé tlačítko je stisknuto/podrženo, postava vyskočí a sníží svou vertikální rychlost
+                  if not jumping and cubeY == GROUND_Y:
+                        if held:
+                              jumping = True
+                              cubeVelocity = -20
+                  if jumping:
+                        cubeY += cubeVelocity
+                        cubeVelocity += GRAVITY
+                        #pokud se postava dotýká země, postava se nenachází ve vzduchu a vertikální rychlost je vynulována
+                        if cubeY >= GROUND_Y:
+                              cubeY = GROUND_Y
+                              jumping = False
+                              cubeVelocity = 0    
+                  if obstX < 0:
+                        obstX = DISP_SIZE[0]
+                  
+                  
+                  #zobrazení všech elementů na obrazovku
                   obstX -= 10
                   sky = disp.blit(skyImage, (0,0)) #pozadí
-                  
                   pygame.draw.rect(disp, GND_COLOR, groundRect) #hlína
                   pygame.draw.rect(disp, OVERLAY_GND_COLOR, groundOverlay) #tráva
                   virus = disp.blit(pygame.transform.scale(virusImage, (64,64)), (obstX, GROUND_Y))
                   cube = disp.blit(cubeImage, (cubeX,cubeY)) #hráčova postava
 
-            if cube.colliderect(virus):
-                  resButton = pygame.Rect(100,270,150,100)  
-                  disp.fill(BG_COLOR)
-                  text = font.render('Game over', True, (255,0,0))
-                  reset = pygame.draw.rect(disp, (0,255,255), resButton)
-                  disp.blit(text, (DISP_SIZE[0]//2, DISP_SIZE[1]//2))
-                  dead = True
-                  inMenu = True
+                  if cube.colliderect(virus):
+                        running = False
+                        mainMenu(score, True)
 
-            pygame.display.update()
-            gameClock.tick(FRAMERATE)
+                  pygame.display.update()
+                  gameClock.tick(FRAMERATE)
+            pygame.quit()
+      def mainMenu(score=0, death=False):
+            menu = pygame_menu.Menu('Just Jump', DISP_SIZE[0], DISP_SIZE[1], theme=pygame_menu.themes.THEME_BLUE)
+            if death:
+                  menu.add.label(f'You died!')
+            menu.add.label(f'Score: {score}')
+            menu.add.button('Play', game)
+            menu.add.button('Quit', pygame_menu.events.EXIT)
+            menu.mainloop(disp)
+            
+      mainMenu()
 
-      pygame.quit()
 
 
 
