7403eed9053fa56aaf529321c9075f14a5f7dcc9
giimiks <fakinafakuka@gmail.com>
2024-04-16 13:28:59 +0200

    double obstacles implemented

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ JUMP_HEIGHT = 140  # výška skoku
 GRAVITY = 1.75  # gravitace
 
 
+
 #funkce main; pokud bych jinde importoval main.py, 
 #nespustí se mi hra znovu
 def main():
@@ -32,6 +33,8 @@ def main():
       skyImage = pygame.image.load('assets/sky.png')
       cubeImage = pygame.image.load('assets/cube.png')
       shieldImage = pygame.image.load('assets/shield.png')
+      scoreFont = pygame.font.SysFont('Arial', 30)
+
       def game():
             
             pygame.font.init()
@@ -41,8 +44,8 @@ def main():
             obstacles = [] #seznam překážek
             lastSpawnTime = time.time()
             spawnRate = 5
-            score = 0
-
+            score = 5000
+      
 
             
             groundRect = pygame.Rect(0,DISP_SIZE[1]-160,DISP_SIZE[0],160)
@@ -56,21 +59,25 @@ def main():
             running = True
             held = False
             shielded = False
+            shieldAble = True
             lastShieldTime = 0
             #hlavní smyčka hry
             while running:
+                  #print(shieldAble)
                   score+=1
                   currentTime = time.time()
                   dt = currentTime - lastSpawnTime
+                  shieldX, shieldY = cubeX-16, cubeY-16
                   if lastShieldTime + 0.75 < currentTime:
                         shielded = False
+                        shieldX, shieldY = -1000,-1000
                   spawnRate = max(1,2-score/500)
 
                   if dt > spawnRate:
                         lastSpawnTime = currentTime
-                        spawnObstacle(obstacles)
+                        spawnObstacle(obstacles, score)
                   
-                  updateObstacles(obstacles)
+                  updateObstacles(obstacles, score)
 
                  
 
@@ -85,14 +92,16 @@ def main():
                                     if shieldBtnRect.collidepoint(pygame.mouse.get_pos()):
                                           
                                           print(currentTime - lastShieldTime)
-                                          if not shielded:
+                                          if not shielded and shieldAble:
                                                 lastShieldTime = currentTime
                                                 shielded = True
+                                                shieldAble = False
                                     held = False
                         elif pygame.key.get_pressed()[pygame.K_e]:
-                              if not shielded:
+                              if not shielded and shieldAble:
                                                 lastShieldTime = currentTime
                                                 shielded = True
+                                                shieldAble = False
                         elif pygame.key.get_pressed()[pygame.K_ESCAPE]:
                               mainMenu(score, death=False)
                   #pokud hráč není ve vzduchu a levé tlačítko je stisknuto/podrženo, postava vyskočí a sníží svou vertikální rychlost
@@ -108,7 +117,7 @@ def main():
                               jumping = False
                               cubeVelocity = 0    
                   
-                  shieldX, shieldY = cubeX-16, cubeY-16
+                  
                   #zobrazení všech elementů na obrazovku
                   sky = disp.blit(skyImage, (0,0)) #pozadí
                   pygame.draw.rect(disp, GND_COLOR, groundRect) #hlína
@@ -118,11 +127,18 @@ def main():
                   if shielded: 
                         shieldBlit = disp.blit(pygame.transform.scale(shieldImage, (96,96)), (shieldX,shieldY))
                   else:
-                        pygame.draw.rect(disp, (240,0,0), shieldBtnRect)
-                        shieldBlit = disp.blit(pygame.transform.scale(shieldImage, (96,96)), (-10000,-10000))
+                        if shieldAble:
+                              pygame.draw.rect(disp, (240,0,0), shieldBtnRect)
+                              shieldBlit = disp.blit(pygame.transform.scale(shieldImage, (96,96)), (-10000,-10000))
                   for obstacle in obstacles:
-                        disp.blit(pygame.transform.scale(virusImage, (64,64)), obstacle['rect'])
-                  checkShield(shieldBlit, obstacles)
+                        if not (type(obstacle) is list):
+                              disp.blit(pygame.transform.scale(virusImage, (64,64)), obstacle['rect'])
+                        else:
+                              for ob in obstacle:
+                                    disp.blit(pygame.transform.scale(virusImage, (64,64)), ob['rect'])
+                  showsScore = scoreFont.render("Score: " + str(score), True, (255, 255, 255))
+                  disp.blit(showsScore, (DISP_SIZE[0]/2, 50))
+                  shieldAble = checkShield(shieldBlit, obstacles, shieldAble, shielded)
                   checkCollision(cube, obstacles, score)
                   pygame.display.update()
                   gameClock.tick(FRAMERATE)
@@ -136,36 +152,66 @@ def main():
             menu.add.button('Quit', pygame_menu.events.EXIT)
             menu.mainloop(disp)
 
-      def spawnObstacle(obstacles: list):
+      def spawnObstacle(obstacles: list, score: int):
             spawnPos = randint(0,2) #0 nahoře, 1, vlevo, 2 vpravo
+            spawnNum = 1
+            if score > 5000:
+                  spawnNum = 2
             if spawnPos == 0:
                   obstRect = pygame.Rect(720//3, -50, 50, 50)
-                  obstacles.append({'rect': obstRect, 'speed': 10, 'type': 'top'})
+                  obstacles.append([{'rect': obstRect, 'speed': 10, 'type': 'top'}])
+                  print(obstacles)
             elif spawnPos == 1:
-                  obstRect = pygame.Rect(-50, 340, 50, 50)
-                  obstacles.append({'rect': obstRect, 'speed': 10, 'type': 'right'})
+                  
+                  obstRect = pygame.Rect(-100, 340, 50, 50)
+                  obstacles.append([{'rect': obstRect, 'speed': 10, 'type': 'left'}])
+                  print(obstacles)
             elif spawnPos == 2:
-                  obstRect = pygame.Rect(720, 340, 50, 50)
-                  obstacles.append({'rect': obstRect, 'speed': -10, 'type': 'left'})
+                  if spawnNum != 2:
+                        obstRect = pygame.Rect(720, 340, 50, 50)
+
+                        obstacles.append([{'rect': obstRect, 'speed': -10, 'type': 'right'}])
+                  else:
+                        obstRect = pygame.Rect(720, 340, 50, 50)
+                        obstRect2 = pygame.Rect(770, 340, 50, 50)
+                        obstacles.append([{'rect': obstRect, 'speed': -10, 'type': 'right'},{'rect': obstRect2, 'speed': -10, 'type': 'right'}])
+                  print(obstacles)
+
+                  
       
-      def updateObstacles(obstacles):
+      def updateObstacles(obstacles, score):
             for obstacle in obstacles:
-                  if obstacle['type'] == 'left' or obstacle['type'] == 'right':
-                        obstacle['rect'].move_ip(obstacle['speed'], 0)
-                  elif obstacle['type'] == 'top':
-                        obstacle['rect'].move_ip(0, obstacle['speed'])
-                  if obstacle['rect'].right < 0 or obstacle['rect'].left > DISP_SIZE[0] or obstacle['rect'].top > 540:
-                        obstacles.remove(obstacle)
+                  if obstacle[0]['type'] == 'left':
+                        obstacle[0]['rect'].move_ip(obstacle[0]['speed']+score/1000, 0)
+                  elif  obstacle[0]['type'] == 'right':
+                        obstacle[0]['rect'].move_ip(obstacle[0]['speed']-score/1000, 0)
+                        if len(obstacle) == 2:
+                              obstacle[1]['rect'].move_ip(obstacle[0]['speed']-score/1000, 0)
+                        
+                  elif obstacle[0]['type'] == 'top':
+                        obstacle[0]['rect'].move_ip(0, obstacle[0]['speed']+score/1000)
+                  if (obstacle[0]['type'] == 'left' and  obstacle[0]['rect'].left > DISP_SIZE[0] )or (obstacle[0]['type'] == 'right' and obstacle[0]['rect'].left < 0) or obstacle[0]['rect'].top > 540:
+                        if len(obstacle) == 2:
+                              if obstacle[1]['rect'].left < 0:
+                                    obstacles.remove(obstacle)
+                              else:
+                                    obstacles.remove(obstacle)
 
       def checkCollision(player, obstacles, score):
             for obstacle in obstacles:
-                  if player.colliderect(obstacle['rect']):
+                  if player.colliderect(obstacle[0]['rect']):
                         mainMenu(score, death=True)
+                  if len(obstacle) == 2:
+                        if player.colliderect(obstacle[1]['rect']):
+                              mainMenu(score, death=True)
 
-      def checkShield(shield, obstacles):
+      def checkShield(shield, obstacles, shieldable, shielded):
             for obstacle in obstacles:
-                  if obstacle['type'] == 'top' and shield.colliderect(obstacle['rect']):
+                  if obstacle[0]['type'] == 'top' and shield.colliderect(obstacle[0]['rect']) and shielded:
                         obstacles.remove(obstacle)
+                        
+                        return True
+            return shieldable
 
       mainMenu()
 
